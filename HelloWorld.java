82cf2ebfb34c9d069a13549fa019484a0564ee76
Jose Antonio Rosales Giron <joseantrosalesprofesional@gmail.com>
2019-09-25 10:30:49 +0200

    Uploading Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+*/
+public class HelloWorld { // Clase principal
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
